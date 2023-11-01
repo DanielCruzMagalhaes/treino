ec0d35aabd4970fe7b89ebc21318996cb0aaa46e
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-10-31 21:55:51 -0300

    criada urls.py

--- dan/projet/aplicativo/models.py
+++ dan/projet/aplicativo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    publicação = models.DateField()
+    paginas = models.IntegerField()
--- dan/projet/aplicativo/views.py
+++ dan/projet/aplicativo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Livro
+
+
+
+def lista_livros(request):
+    livros = Livro.object.all()
+    return render(request, 'lista_livros.html',{'livros': livros})
 
 # Create your views here.
